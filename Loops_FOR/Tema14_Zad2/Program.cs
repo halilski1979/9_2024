fd1b3e05c6ed1fa0663b712936018502a82fc19c
Halil <halilski1979@gmail.com>
2024-03-12 09:32:17 +0200

    esfsf

---
+++ Loops_FOR/Tema14_Zad2/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Tema14_Zad2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int max = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                sum += num;
+
+                if (max<num)
+                {
+                    max= num;
+                }
+            }
+            
+            if (sum-max==max)
+            {
+                Console.WriteLine("Yes!");
+                Console.WriteLine($"Sum={max}");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+                Console.WriteLine($"Diff={Math.Abs(max-(sum-max))}");
+            }
+           
+            
+        }
+    }
+}
---
+++ Loops_FOR/Tema14_Zad5/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Tema14_Zad5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=int.Parse(Console.ReadLine());
+
+            double count1 = 0;
+            double count2 = 0;
+            double count3 = 0;
+           
+
+            
+            for (int i = 0; i < n; i++)         
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (num%2==0)
+                {
+                    count1++;
+                }
+                if (num%3==0)
+                {
+                    count2++;
+                }
+                 if (num%4==0)
+                {
+                    count3++;
+                }
+                
+            }
+
+            Console.WriteLine($"{((count1/n)*100):f2}%");
+            Console.WriteLine($"{((count2 / n) * 100):f2}%");
+            Console.WriteLine($"{(count3 / n) * 100:f2}%");
+            
+
+
+        }
+    }
+}
